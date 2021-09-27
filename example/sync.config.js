e9a8fab04b36196079ff3f7579044fe8f27f55de
林志鹏 <zhipenglin@knx.com.cn>
2021-09-27 10:05:52 +0800

    删除不必要文件

--- example/sync.config.js
+++ example/sync.config.js
@@ -6,26 +6,7 @@ module.exports = {
     basDir: 'src/',
     targetDir: '/talent-view',
     saveFile: async ({path, content}) => {
-        if (!cookies) {
-            const res = await axios.post('http://lite.knx.com/api/v1/user/login', {
-                username: '13120827721',
-                type: 'password',
-                token: Number.parseInt(Math.random() * 100000).toString(),
-                password: 'e10adc3949ba59abbe56e057f20f883e'
-            });
-            cookies = res.headers['set-cookie'].map((str) => str.split(';')[0]).join(';')
-        }
-        const res = await axios.post('http://lite.knx.com/api/v1/admin/view/autoCreate', {
-            path,
-            content: JSON.parse(content)
-        }, {
-            headers: {
-                "Content-Type": "application/json",
-                "x-app-code": "RECRUITMENT",
-                "Cookie": cookies
-            }
-        });
-
-        return res.data;
+        console.log(path, content);
+        return {};
     }
 };
